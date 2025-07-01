038f9a01d00c05cd18d89ae1129811d2c2e29ffe
lucasgbom <lucasgabrielprofs@gmail.com>
2025-07-01 16:18:47 -0300

    01/07/2025

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,14 @@
 extends CharacterBody2D
-
+@onready var sprite: Sprite2D = $Sprite2D
 @export var speed = 500.0
-var screen_size
-
-func ready():
-	screen_size = get_viewport_rect().size
 
 func _process(delta):
+	#movimento
 	var direction = Input.get_vector("left", "right", "up", "down")
 	position += direction.normalized() * speed * delta
+	#flip do sprite
+	sprite.flip_h = get_global_mouse_position().x < position.x
+	#tiro
+	if (Input.is_action_just_pressed("shoot")):
+		print("shoot")
 	

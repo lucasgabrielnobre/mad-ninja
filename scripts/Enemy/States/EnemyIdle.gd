aa94452acc364c004b848a01af430171eb9c7eba
lucasgbom <lucasgabrielprofs@gmail.com>
2025-07-22 20:25:28 -0300

    8.2

--- scripts/Enemy/States/EnemyIdle.gd
+++ scripts/Enemy/States/EnemyIdle.gd
@@ -44,6 +44,7 @@ func field_of_view():
 			ray_cast.target_position = direction
 			if ray_cast.is_colliding():
 				if ray_cast.get_collider() == player and ray_cast.target_position.length() < view_distance:
+					alert_nearby_enemies(get_nearby_enemies(enemy, 8000.0))
 					Transitioned.emit(self, "chase")
 func handle_gun():
 	var angle = move_direction.angle()
@@ -57,5 +58,25 @@ func handle_gun():
 	gun.position = offset
 	gun.rotation = offset.angle()
 		
+func get_nearby_enemies(origin: CharacterBody2D, radius: float = 400.0) -> Array:
+	var enemies_in_radius = []
+	var all_enemies = get_tree().get_nodes_in_group("enemies")  # Certifique-se de adicionar seus inimigos a esse grupo
+	
+	for enemy in all_enemies:
+		if enemy == origin:  # Evita considerar o próprio personagem (caso esteja no grupo)
+			continue
+		
+		if origin.global_position.distance_to(enemy.global_position) <= radius:
+			enemies_in_radius.append(enemy)
+	
+	return enemies_in_radius
 
 	
+func alert_nearby_enemies(nearby : Array):
+	var i = 0
+	for en in nearby:
+		i += 1
+		print(str(i) + ": " + str(en))
+		var current_state = en.get_node("StateMachine").current_state
+		if current_state:
+			current_state.Transitioned.emit(current_state, "chase")
